a2da250af64fa1cd13927ffb8c31929706d5342a
pentarra <70299526+pentarra@users.noreply.github.com>
2020-08-27 04:20:52 +0300

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,7 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('grappelli/', include('grappelli.urls')),
     path('admin/', admin.site.urls),
     path('polls/', include('polls.urls')), 
     path('articles/', include('articles.urls')),
